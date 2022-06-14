53663ffd7f9cf2780f4e627ecffb0c16fb8d7d1a
aashishupreti <aashishupreti111@gmail.com>
2022-06-14 10:46:38 +0545

    initial phase

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.scss'
+// import { images } from '../../constants'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        {/* <img src={images.logo} alt="logo" /> */}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
